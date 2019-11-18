4b46c85cf2d0076a10efaf6e58d9097241164c7c
ROBOT_SHO <soarbear7@gmail.com>
2019-11-18 17:02:17 +0900

    Update kNN.py

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -28,22 +28,7 @@ def classify0(inX, dataSet, labels, k):
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
-
-def file2matrix(filename):
-    fr = open(filename)
-    numberOfLines = len(fr.readlines())         #get the number of lines in the file
-    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
-    classLabelVector = []                       #prepare labels return   
-    fr = open(filename)
-    index = 0
-    for line in fr.readlines():
-        line = line.strip()
-        listFromLine = line.split('\t')
-        returnMat[index,:] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
-        index += 1
-    return returnMat,classLabelVector
-       
+     
 def img2vector(filename):
     returnVect = zeros((1,1024))
     fr = open(filename)
