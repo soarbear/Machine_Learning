11a42944a27506489a33252424258f71db842dc5
ROBOT_SHO <soarbear7@gmail.com>
2019-12-03 20:41:50 +0900

    Update adaboost.py

--- adaboost/adaboost.py
+++ adaboost/adaboost.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def loadDataset():
-    datMat = matrix([[ 1. ,  2.1],
+    datMat = np.matrix([[ 1. ,  2.1],
         [ 2. ,  1.1],
         [ 1.3,  1. ],
         [ 1. ,  1. ],
@@ -29,19 +29,18 @@ def loadDataSet(fileName):      #general function to parse tab -delimited floats
     return dataMat,labelMat
 
 def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):#just classify the data
-    retArray = ones((shape(dataMatrix)[0],1))
+    retArray = -np.ones((np.shape(dataMatrix)[0],1))
     if threshIneq == 'lt':
-        retArray[dataMatrix[:,dimen] <= threshVal] = -1.0
+        retArray[dataMatrix[:,dimen] <= threshVal] = 1.0
     else:
-        retArray[dataMatrix[:,dimen] > threshVal] = -1.0
+        retArray[dataMatrix[:,dimen] > threshVal] = 1.0
     return retArray
-    
 
 def buildStump(dataArr,classLabels,D):
-    dataMatrix = mat(dataArr); labelMat = mat(classLabels).T
-    m,n = shape(dataMatrix)
-    numSteps = 10.0; bestStump = {}; bestClasEst = mat(zeros((m,1)))
-    minError = inf #init error sum, to +infinity
+    dataMatrix = np.mat(dataArr); labelMat = np.mat(classLabels).T
+    m,n = np.shape(dataMatrix)
+    numSteps = 10.0; bestStump = {}; bestClasEst = np.mat(np.zeros((m,1)))
+    minError = np.inf #init error sum, to +infinity
     for i in range(n):#loop over all dimensions
         rangeMin = dataMatrix[:,i].min(); rangeMax = dataMatrix[:,i].max();
         stepSize = (rangeMax-rangeMin)/numSteps
@@ -49,7 +48,7 @@ def buildStump(dataArr,classLabels,D):
             for inequal in ['lt', 'gt']: #go over less than and greater than
                 threshVal = (rangeMin + float(j) * stepSize)
                 predictedVals = stumpClassify(dataMatrix,i,threshVal,inequal)#call stump classify with i, j, lessThan
-                errArr = mat(ones((m,1)))
+                errArr = np.mat(np.ones((m,1)))
                 errArr[predictedVals == labelMat] = 0
                 weightedError = D.T*errArr  #calc total error multiplied by D
                 #print "split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError)
@@ -63,39 +62,39 @@ def buildStump(dataArr,classLabels,D):
 
 def adaBoostTrainDS(dataArr,classLabels,numIt=40):
     weakClassArr = []
-    m = shape(dataArr)[0]
-    D = mat(ones((m,1))/m)   #init D to all equal
-    aggClassEst = mat(zeros((m,1)))
+    m = np.shape(dataArr)[0]
+    D = np.mat(np.ones((m,1))/m)   #init D to all equal
+    aggClassEst = np.mat(np.zeros((m,1)))
     for i in range(numIt):
         bestStump,error,classEst = buildStump(dataArr,classLabels,D)#build Stump
         #print(f"D: {D.T}")
-        alpha = float(0.5*log((1.0-error)/max(error,1e-16)))#calc alpha, throw in max(error,eps) to account for error=0
+        alpha = float(0.5*np.log((1.0-error)/max(error,1e-16)))#calc alpha, throw in max(error,eps) to account for error=0
         bestStump['alpha'] = alpha  
         weakClassArr.append(bestStump)                  #store Stump Params in Array
         #print(f"classEst: {classEst.T}")
-        expon = multiply(-1*alpha*mat(classLabels).T,classEst) #exponent for D calc, getting messy
-        D = multiply(D,exp(expon))                              #Calc New D for next iteration
+        expon = np.multiply(-1*alpha*np.mat(classLabels).T,classEst) #exponent for D calc, getting messy
+        D = np.multiply(D,np.exp(expon))                              #Calc New D for next iteration
         D = D/D.sum()
         #calc training error of all classifiers, if this is 0 quit for loop early (use break)
         aggClassEst += alpha*classEst
         #print(f"aggClassEst:, {aggClassEst.T})
-        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T,ones((m,1)))
+        aggErrors = np.multiply(np.sign(aggClassEst) != np.mat(classLabels).T,np.ones((m,1)))
         errorRate = aggErrors.sum()/m
         #print(f"total error: {errorRate}")
         if errorRate == 0.0: break
     return weakClassArr,aggClassEst
 
 def adaClassify(datToClass,classifierArr):
-    dataMatrix = mat(datToClass)#do stuff similar to last aggClassEst in adaBoostTrainDS
-    m = shape(dataMatrix)[0]
-    aggClassEst = mat(zeros((m,1)))
+    dataMatrix = np.mat(datToClass)#do stuff similar to last aggClassEst in adaBoostTrainDS
+    m = np.shape(dataMatrix)[0]
+    aggClassEst = np.mat(np.zeros((m,1)))
     for i in range(len(classifierArr)):
         classEst = stumpClassify(dataMatrix,classifierArr[i]['dim'],\
                                  classifierArr[i]['thresh'],\
                                  classifierArr[i]['ineq'])#call stump classify
         aggClassEst += classifierArr[i]['alpha']*classEst
         #print(f"aggClassEst: {aggClassEst}")
-    return sign(aggClassEst)
+    return np.sign(aggClassEst)
 
 def showDataset(dataMat, labelMat):
     data_plus = []
@@ -115,4 +114,4 @@ if __name__ == '__main__':
     dataArr,classLabels = loadDataset()
     showDataset(dataArr,classLabels)
     weakClassArr, aggClassEst = adaBoostTrainDS(dataArr, classLabels)
-    print(f"[INFO]point[-1,0], point[3,4] classified as: {int(adaClassify([[0,0],[5,5]], weakClassArr)[0,0])}, {int(adaClassify([[0,0],[5,5]], weakClassArr)[1,0])}")
+    print(f"[INFO]point[-1,0], point[3,4] classified as: {int(adaClassify([[-1,0],[3,4]], weakClassArr)[0,0])}, {int(adaClassify([[-1,0],[3,4]], weakClassArr)[1,0])}")
