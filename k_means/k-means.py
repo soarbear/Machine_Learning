58653549ad0796103acb407693e526356725bb5b
ROBOT_SHO <soarbear7@gmail.com>
2019-12-09 12:32:21 +0900

    Create k-means.py

---
+++ k_means/k-means.py
@@ -0,0 +1,56 @@
+'''
+Created on Feb 16, 2011
+k Means Clustering for Ch10 of Machine Learning in Action
+@author: Peter Harrington
+'''
+from numpy import *
+
+def loadDataSet(fileName):      #general function to parse tab -delimited floats
+    dataMat = []                #assume last column is target value
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        #fltLine = map(float,curLine) #map all elements to float()
+        fltLine = list(map(float,curLine)) #map all elements to float()
+        dataMat.append(fltLine)
+    return dataMat
+
+def distEclud(vecA, vecB):
+    return sqrt(sum(power(vecA - vecB, 2))) #la.norm(vecA-vecB)
+
+def randCent(dataSet, k):
+    n = shape(dataSet)[1]
+    centroids = mat(zeros((k,n)))#create centroid mat
+    for j in range(n):#create random cluster centers, within bounds of each dimension
+        minJ = min(dataSet[:,j]) 
+        rangeJ = float(max(dataSet[:,j]) - minJ)
+        centroids[:,j] = mat(minJ + rangeJ * random.rand(k,1))
+    return centroids
+    
+def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m,2)))#create mat to assign data points 
+                                      #to a centroid, also holds SE of each point
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):#for each data point assign it to the closest centroid
+            minDist = inf; minIndex = -1
+            for j in range(k):
+                distJI = distMeas(centroids[j,:],dataSet[i,:])
+                if distJI < minDist:
+                    minDist = distJI; minIndex = j
+            if clusterAssment[i,0] != minIndex: clusterChanged = True
+            clusterAssment[i,:] = minIndex,minDist**2
+        print(f'centroids:\n{centroids}')
+        for cent in range(k):#recalculate centroids
+            ptsInClust = dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]#get all the point in this cluster
+            centroids[cent,:] = mean(ptsInClust, axis=0) #assign centroid to mean 
+    return centroids, clusterAssment
+
+if '__main__' == __name__:
+    datafile = '/content/drive/My Drive/Colab Notebooks/Machine_Learning/k_means/testSet.txt'
+    datMat = mat(loadDataSet(datafile))
+    centroids, clustAssment = kMeans(datMat, 4)
+    #print(f'centroids: {centroids}')
